f97e83b565f51daf7510bc05ba2547881ba23448
sarmedrizvi <sarmedrizvi@gmail.com>
2020-05-14 04:55:54 +0500

    Search Bar is now working,Every function is working according to the rubric

--- src/App.js
+++ src/App.js
@@ -14,12 +14,23 @@ class App extends React.Component {
      * pages, as well as provide a good URL they can bookmark and share.
      */
   };
-
+  formRef = React.createRef();
   render() {
     return (
       <div className="app">
-        <Route exact path="/" component={BooksApp} />
-        <Route path="/search" component={SearchBook} />
+        <Route
+          exact
+          path="/"
+          component={() => <BooksApp ref={this.formRef} />}
+        />
+        <Route
+          path="/search"
+          component={() => (
+            <SearchBook
+              AppBooks={this.formRef.current && this.formRef.current.state}
+            />
+          )}
+        />
       </div>
     );
   }
--- src/Components/SearchBook.js
+++ src/Components/SearchBook.js
@@ -1,21 +1,85 @@
 import React, { Component } from "react";
 // import PropTypes from "prop-types";
 import "../App.css";
-import { search } from "../BooksAPI";
+import { search, update } from "../BooksAPI";
 import { Link } from "react-router-dom";
 
 export default class SearchBook extends Component {
-  //   static propTypes = { prop: PropTypes };
-  state = { search: "", searchBooks: [] };
-  enterEvent = (event) => {
-    if (event.key === "Enter") {
-      search(this.state.search).then((res) => {
-        this.setState({ searchBooks: res });
-        console.log(this.state.searchBooks);
-      });
-    }
+  state = { books: [], searchValue: "", selectedOption: "", searchBooks: [] };
+  selectChange = (value, book) => {
+    this.setState({ selectedOption: value.target.value }, () => {
+      // console.log(this.state.selectedOption);
+      update(book, this.state.selectedOption);
+    });
   };
+  componentDidMount() {
+    const appBooks = this.props.AppBooks;
+    if (appBooks) {
+      this.setState(
+        {
+          books: [
+            ...appBooks.read,
+            ...appBooks.wantToRead,
+            ...appBooks.currentlyReading,
+          ],
+        },
+        () => {
+          // console.log(this.state.books);
+        }
+      );
+    }
+
+    // for (const book in this.props.AppBooks) {
+    //   // this.setState(
+    //   //   (prev) => {
+    //   //     return { books: prev.books.push(this.props.AppBooks[book]) };
+    //   //   },
+    //   //   () => {
+    //   //     console.log(this.state.books);
+    //   //   }
+    //   // );
+    // }
+  }
+
   render() {
+    const TextChangeEvent = (event) => {
+      this.setState({
+        searchValue: event.target.value,
+      });
+      if (this.state.searchValue === "") {
+        this.setState({ searchBooks: [] });
+        return;
+      }
+      search(this.state.searchValue)
+        .then((res) => {
+          if (res && !res.error)
+            this.filtered = res.map((book) => {
+              const check = this.state.books.find(
+                (Checkbook) => Checkbook.id === book.id
+              );
+              if (check) {
+                return { ...book, shelf: check.shelf };
+              } else {
+                return book;
+              }
+            });
+
+          this.setState({ searchBooks: this.filtered }, () => {
+            if (this.state.searchValue === "") {
+              this.setState({
+                searchBooks: [],
+              });
+              return;
+            }
+          });
+
+          // console.log(this.state.searchBooks);
+        })
+        .catch((err) => {
+          console.log(err);
+          this.setState({ searchBooks: [] });
+        });
+    };
     return (
       <div className="search-books">
         <div className="search-books-bar">
@@ -33,52 +97,60 @@ export default class SearchBook extends Component {
                 */}
             <input
               type="text"
-              value={this.state.search}
               placeholder="Search by title or author"
-              onKeyDown={this.enterEvent}
-              onChange={(event) =>
-                this.setState({ search: event.target.value })
-              }
+              onChange={TextChangeEvent}
+              value={this.state.searchValue}
             />
           </div>
         </div>
         <div className="search-books-results">
           <ol className="books-grid">
-            {this.state.searchBooks.map((book) => {
-              return (
-                <li key={book.id}>
-                  <div className="book">
-                    <div className="book-top">
-                      <div
-                        className="book-cover"
-                        style={{
-                          width: 128,
-                          height: 193,
-                          backgroundImage: `url(${
-                            book.imageLinks.smallThumbnail
-                          })`,
-                        }}
-                      />
-                      <div className="book-shelf-changer">
-                        <select>
-                          <option value="move" disabled>
-                            Move to...
-                          </option>
-                          <option value="currentlyReading">
-                            Currently Reading
-                          </option>
-                          <option value="wantToRead">Want to Read</option>
-                          <option value="read">Read</option>
-                          <option value="none">None</option>
-                        </select>
+            {this.state.searchBooks.length !== 0 ? (
+              this.state.searchBooks.map((book) => {
+                return (
+                  <li key={book.id}>
+                    <div className="book">
+                      <div className="book-top">
+                        <div
+                          className="book-cover"
+                          style={{
+                            width: 128,
+                            height: 193,
+                            backgroundImage: `url(${book.imageLinks &&
+                              book.imageLinks.smallThumbnail})`,
+                          }}
+                        />
+                        <div className="book-shelf-changer">
+                          <select
+                            onChange={(event) => this.selectChange(event, book)}
+                            value={book.shelf}
+                          >
+                            <option value="move" disabled>
+                              Move to...
+                            </option>
+                            <option value="currentlyReading">
+                              Currently Reading
+                            </option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                            <option value="none">None</option>
+                          </select>
+                        </div>
                       </div>
+                      <div className="book-title">{book.title}</div>
+                      {book.authors &&
+                        book.authors.map((author) => (
+                          <div key={author} className="book-authors">
+                            {author}
+                          </div>
+                        ))}
                     </div>
-                    <div className="book-title">{book.title}</div>
-                    <div className="book-authors">{book.subtitle}</div>
-                  </div>
-                </li>
-              );
-            })}
+                  </li>
+                );
+              })
+            ) : (
+              <li />
+            )}
           </ol>
         </div>
       </div>
